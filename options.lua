647fffa1e81053721866d83d4817b9efba0c2503
NLshaen <yan.lucas.free.fr>
2025-03-29 14:11:25 +0100

    Fix release.yml and add pkgmeta

--- options.lua
+++ options.lua
@@ -3,7 +3,7 @@ local AFKMod, E, L, V, P, G = unpack(select(2, ...))
 
 function AFKMod:SetupOptions()
 	-- Register plugin so options are properly inserted when config is loaded
-		print("|cff00ff00ElvUI_AFKMod SetupOptions !!!|r")
+		print("|cff00ff00ElvUI_AFKMod Setup Options !!!|r")
 end
 
 -- Register the module with ElvUI. ElvUI will now call MyPlugin:Initialize() when ElvUI is ready to load our plugin.
